54b97f20ac6d1980ce894b89db0e5843bda4d2c0
tomMoulard <tom.moulard@epita.fr>
2018-02-09 13:03:46 +0100

    adding a proper arguments parsing (argparse)

--- generator.py
+++ generator.py
@@ -14,6 +14,7 @@ import genetik
 def main():
     keyboard = keyBoard.KeyBoard("qwerty", seed=42)
     keyboard.toGraph()
+    print(keyboard.graph.toDot())
     
 def play(genNumber, popSize, fileName):
     pop = [keyBoard.KeyBoard("random", seed=42) for x in range(popSize)]
@@ -22,6 +23,25 @@ def play(genNumber, popSize, fileName):
         genetik.evolve(pop, fileName)
         print(pop[0], gen)# Print the best keyboard for each generation
 
-if __name__ == '__main__':
-    main()
-    
+if __name__ == "__main__":
+    import argparse
+    import sys
+    parser = argparse.ArgumentParser("python3 " + str(sys.argv[0]))
+    parser.add_argument("klf",
+                        metavar="KLF", type=argparse.FileType("r+"), default="./keys.log",
+                        help="to add the keylogger file (KLF) to train the programm to")
+    parser.add_argument("--len",
+                        metavar="nb", type=int, default=10,
+                        help="represent the lenght of the learning")
+    parser.add_argument("--ps",
+                        metavar="nb", type=int, default=10,
+                        help="represent the population size to evolve")
+    parser.add_argument("--seed",
+                        metavar="seed", type=int, default=42,
+                        help="to set the seed")
+    parser.add_argument("--verbose", "-v", action="count", default=0,
+                        help="add as many as you want to set the verbose level")
+    args = parser.parse_args()
+    print(args.klf.readlines())
+    print("len: {}, popsize:{}, seed: {}, verbose: {}".format(args.len, args.ps, args.seed, args.verbose))
+    # main()
