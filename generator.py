3e1f51a602aa93d66cda875730df82fd710dcda5
Tom Moulard <tom.moulard@epita.fr>
2019-03-09 13:17:27 +0100

    generator: adding init_kb function and a proper args parsing function Also, adding a shebang

--- generator.py
+++ generator.py
@@ -1,3 +1,4 @@
+#!/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on tue Jun 8 20:00:00 2017
@@ -10,18 +11,26 @@ This uses everithing he can to create a powerfull keyboard
 import usefullFunk
 import keyBoard
 import genetik
+import argparse
+import sys
 
-def main(args):
+def init_kb(args):
     keyboard = keyBoard.KeyBoard("qwerty", seed=args.seed)
     keyboard.toGraph()
-    print(keyboard.graph.toDot())
+    if (args.show_dot):
+        print(keyboard.graph.toDot())
+        raise SystemExit(0)
+    return keyboard
+
+def main(args):
+    keyboard = init_kb(args)
     # print(keyboard.getAllLetters())
-    for x in range(len(keyboard.keys)):
-        print(x, keyboard.keys[x])
-    print(keyboard)
-    keyboard.randomize()
-    print(keyboard)
-    
+    #for x in range(len(keyboard.keys)):
+    #    print(x, keyboard.keys[x])
+    #print(keyboard)
+    #keyboard.randomize()
+    #print(keyboard)
+
 def play(genNumber, popSize, fileName):
     pop = [keyBoard.KeyBoard("random", seed=42) for x in range(popSize)]
     genetik.sortPop(pop, 0, popSize - 1)
@@ -29,25 +38,30 @@ def play(genNumber, popSize, fileName):
         genetik.evolve(pop, fileName)
         print(pop[0], gen)# Print the best keyboard for each generation
 
-if __name__ == "__main__":
-    import argparse
-    import sys
-    parser = argparse.ArgumentParser("python3 " + str(sys.argv[0]))
+def parse_args():
+    parser = argparse.ArgumentParser(str(sys.argv[0]))
     parser.add_argument("klf",
-                        metavar="KLF", type=argparse.FileType("r+"), default="./keys.log",
-                        help="to add the keylogger file (KLF) to train the programm to")
-    parser.add_argument("--len",
+                        metavar="KLF", type=argparse.FileType("r+"),
+                        default="./keys.log",
+                        help="keylogger file (KLF) to train the programm")
+    parser.add_argument("--len", "-l",
                         metavar="nb", type=int, default=10,
                         help="represent the lenght of the learning")
-    parser.add_argument("--ps",
+    parser.add_argument("--show-dot",
+                        help="Display the dot version of the graph",
+                        action = "store_true")
+    parser.add_argument("--ps", "-p",
                         metavar="nb", type=int, default=10,
                         help="represent the population size to evolve")
-    parser.add_argument("--seed",
+    parser.add_argument("--seed", "-s",
                         metavar="seed", type=int, default=42,
                         help="to set the seed")
     parser.add_argument("--verbose", "-v", action="count", default=0,
-                        help="add as many as you want to set the verbose level")
+                        help="verbose level(default=0, max=100)")
     args = parser.parse_args()
     print(args.klf.readlines())
     print("len: {}, popsize:{}, seed: {}, verbose: {}".format(args.len, args.ps, args.seed, args.verbose))
-    main(args)
+    return args
+
+if __name__ == "__main__":
+    main(parse_args())
