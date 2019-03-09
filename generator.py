12d9f0f530f75484e9d96776b507d9328bb33833
Tom Moulard <tom.moulard@epita.fr>
2019-03-09 14:32:41 +0100

    generator: using my_print and fixing arguments parsing

--- generator.py
+++ generator.py
@@ -13,6 +13,7 @@ import keyBoard
 import genetik
 import argparse
 import sys
+from my_print import my_print
 
 def init_kb(args):
     keyboard = keyBoard.KeyBoard("qwerty", seed=args.seed)
@@ -20,21 +21,23 @@ def init_kb(args):
     if (args.show_dot):
         print(keyboard.graph.toDot())
         raise SystemExit(0)
+    for x in range(len(keyboard.keys)):
+        my_print(args, x, keyboard.keys[x])
+    my_print(args, keyboard)
+    keyboard.randomize()
+    my_print(args, keyboard)
     return keyboard
 
 def main(args):
     keyboard = init_kb(args)
-    # print(keyboard.getAllLetters())
-    #for x in range(len(keyboard.keys)):
-    #    print(x, keyboard.keys[x])
-    #print(keyboard)
-    #keyboard.randomize()
-    #print(keyboard)
+    play(args.generation_number, args.population_size, args.klf)
 
 def play(genNumber, popSize, fileName):
     pop = [keyBoard.KeyBoard("random", seed=42) for x in range(popSize)]
     genetik.sortPop(pop, 0, popSize - 1)
     for gen in range(genNumber):
+        for kb in pop:
+            kb.valuate()
         genetik.evolve(pop, fileName)
         print(pop[0], gen)# Print the best keyboard for each generation
 
@@ -50,17 +53,21 @@ def parse_args():
     parser.add_argument("--show-dot",
                         help="Display the dot version of the graph",
                         action = "store_true")
-    parser.add_argument("--ps", "-p",
+    parser.add_argument("--generation-number", "--gn", "-g",
+                        metavar="nb", type=int, default=10,
+                        help="represent the number of generations to have")
+    parser.add_argument("--population-size", "--ps", "-p",
                         metavar="nb", type=int, default=10,
                         help="represent the population size to evolve")
     parser.add_argument("--seed", "-s",
-                        metavar="seed", type=int, default=42,
+                        metavar="seed", type=int, default=None,
                         help="to set the seed")
     parser.add_argument("--verbose", "-v", action="count", default=0,
                         help="verbose level(default=0, max=100)")
     args = parser.parse_args()
-    print(args.klf.readlines())
-    print("len: {}, popsize:{}, seed: {}, verbose: {}".format(args.len, args.ps, args.seed, args.verbose))
+    my_print(args, args.klf.readlines())
+    my_print(args, "len: {}, popsize:{}, seed: {}, verbose: {}"
+            .format(args.len, args.population_size, args.seed, args.verbose))
     return args
 
 if __name__ == "__main__":
